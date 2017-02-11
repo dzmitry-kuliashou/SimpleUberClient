2cca0e7655dab201b1116f78c95da45774b6f216
Kuler <Kuler@KULERNOTE>
2017-02-11 21:07:38 +0100

    comment

--- MVCTypeScriptReact3.Config/Installers/InstallWindsorRegistrators.cs
+++ MVCTypeScriptReact3.Config/Installers/InstallWindsorRegistrators.cs
@@ -8,7 +8,7 @@ namespace MVCTypeScriptReact3.Config.Installers
     {
         public static void Install(IWindsorContainer container)
         {
-            //it's boolshit for sure :)
+            //it's bulllshit for sure :) . Just a fast temporary solution
             var applicationPhysicalPath = "D:\\Programming\\React\\MVCTypeScriptReact3\\MVCTypeScriptReact3.Config\\bin\\Debug";
 
             container.Install(
